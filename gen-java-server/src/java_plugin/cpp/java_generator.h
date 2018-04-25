681dad7942f76a88a59d0d5f9bed71a2c6570c19
Andy Day <andy.day@nordstrom.com>
2018-04-25 09:41:44 -0700

    file copyright and attribution notice

--- gen-java-server/src/java_plugin/cpp/java_generator.h
+++ gen-java-server/src/java_plugin/cpp/java_generator.h
@@ -1,3 +1,24 @@
+/*
+ * Copyright 2018 Nordstrom, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * NOTICE: Modified from gRPC-java java_generator.h found at:
+ * https://github.com/grpc/grpc-java/blob/master/compiler/src/java_plugin/cpp/java_generator.h
+ */
+
 #ifndef NET_GRPC_COMPILER_JAVA_GENERATOR_H_
 #define NET_GRPC_COMPILER_JAVA_GENERATOR_H_
 
